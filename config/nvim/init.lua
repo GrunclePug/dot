88ad414efffa183811495005a4b0e480bba55dfc
Chad Humphries <chad.humphries@grunclepug.com>
2025-11-07 00:11:08 -0800

    Add nvim dotfiles

---
+++ config/nvim/init.lua
@@ -0,0 +1,10 @@
+require("config.lazy")
+-- require 'kickstart.plugins.debug',
+-- require 'kickstart.plugins.indent_line',
+-- require 'kickstart.plugins.lint',
+-- require 'kickstart.plugins.autopairs',
+-- require 'kickstart.plugins.neo-tree',
+-- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
